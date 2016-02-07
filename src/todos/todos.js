a5d5c3c01a000fa2df4c637cdc71cd563f33fe13
andersonmcook <andersonmcook@gmail.com>
2016-02-07 14:38:46 -0600

    more angular functions

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -2,25 +2,49 @@ export default function ($scope) {
   let params = {
     createHasInput: false
   };
+
   $scope.todos = [
     {
       task: "do dishes",
-      isCompleted: false
+      isCompleted: false,
+      isEditing: false
     },
     {
       task: "walk the dog",
-      isCompleted: true
+      isCompleted: true,
+      isEditing: false
     }
-  ]
+  ];
+
+// gives a strikethrough class to completed task
   $scope.onCompletedClick = todo => {
     todo.isCompleted = !todo.isCompleted;
   };
 
-  $scope.createTask = () => {
-    params.createHasInput = false;
-    $scope.createTask = '';
+// cancel editing
+  $scope.onCancelClick = todo => {
+    todo.isEditing = false;
   };
 
+// saves updated task
+  $scope.updateTask = todo => {
+    todo.task = todo.updatedTask;
+    todo.isEditing = false;
+  };
+
+// edit a task
+  $scope.onEditClick = todo => {
+    todo.isEditing = true;
+    todo.updatedTask = todo.task;
+  };
+
+// create a new task
+  $scope.createTask = () => {
+    params.createHasInput = false;
+    $scope.createTaskInput = '';
+  };
+
+// watch input for two way binding
   $scope.$watch('createTaskInput', val => {
     if (!val && params.createHasInput) {
       $scope.todos.pop();
