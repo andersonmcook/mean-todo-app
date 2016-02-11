c4fffc9cd0d9f7b88e8ac9beb4e030e8673231c2
andersonmcook <andersonmcook@gmail.com>
2016-02-10 21:01:10 -0600

    working

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,51 +1,42 @@
 import _ from 'lodash';
 
-export default function ($scope, todoFactory) {
-  
-  const { updateTask, deleteTask, createTask, watchCreateTaskInput } = todoFactory;
+export default function($scope, todoFactory) {
+    let params = {
+        createHasInput: false
+    };
 
-  let params = {
-    createHasInput: false
-  };
-
-  $scope.todos = [
+    $scope.todos = [
     {
-      task: "do dishes",
-      isCompleted: false,
-      isEditing: false
+        task: 'do dishes',
+        isCompleted: false,
+        isEditing: false
     },
     {
-      task: "walk the dog",
-      isCompleted: true,
-      isEditing: false
+        task: 'walk the dog',
+        isCompleted: true,
+        isEditing: false
     }
-  ];
+    ];
 
-// gives a strikethrough class to completed task
-  $scope.onCompletedClick = todo => {
-    todo.isCompleted = !todo.isCompleted;
-  };
+    todoFactory.getTasks($scope);
 
-// cancel editing
-  $scope.onCancelClick = todo => {
-    todo.isEditing = false;
-  };
+    $scope.onCompletedClick = todo => {
+        todo.isCompleted = !todo.isCompleted;
+    };
 
-// saves updated task
-  $scope.updateTask = _.partial(updateTask);
+    $scope.onEditClick = todo => {
+        todo.isEditing = true;
+        todo.updatedTask = todo.task;
+    };
 
-// edit a task
-  $scope.onEditClick = todo => {
-    todo.isEditing = true;
-    todo.updatedTask = todo.task;
-  };
+    $scope.onCancelClick = todo => {
+        todo.isEditing = false;
+    };
 
-// delete a task
-  $scope.deleteTask = _.partial(deleteTask, $scope);
+    const { createTask, updateTask, deleteTask, watchCreateTaskInput } = todoFactory;
 
-// create a new task
-  $scope.createTask = _.partial(createTask, $scope, params);
-
-// watch input for two way binding
-  $scope.$watch('createTaskInput', _.partial(watchCreateTaskInput, $scope, params));
-};
+    $scope.createTask = _.partial(createTask, $scope, params);
+    $scope.updateTask = _.partial(updateTask, $scope);
+    $scope.deleteTask = _.partial(deleteTask, $scope);
+    $scope.$watch('createTaskInput', _.partial(watchCreateTaskInput, params, $scope));
+}
